ba0923ff0ceec99a91d315c6ec39ef5374fc4a1e
Laura Pasqualone <pasqualone.laura@gmail.com>
2021-06-08 16:56:52 -0700

    Write computerPlay function, edit css

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function computerPlay {
-    
+let playerScore = 0;
+let computerScore = 0;
+
+function computerPlay() {
+    let options = [ 'ROCK', 'PAPER', 'SCISSORS'];
+    return options[Math.floor(Math.random() * options.length)]; 
+}//console.log(computerPlay(options));
+
+let computerSelection = computerPlay('options');
+
+document.getElementById("computerOption").innerHTML = computerPlay();
+
+function playRound() {
+
 }
